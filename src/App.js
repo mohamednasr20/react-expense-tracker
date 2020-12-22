f86e211f14c32c0755910aa653275f4b8cc4e7a2
mohamednasr20 <mohamednasr8687@gmail.com>
2020-12-22 17:16:36 -0500

    add css

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+      </div>
+    </div>
+  );
+}
+
+export default App;
